46a47cbdbea246d9563ac0ecdc5be937d58af856
久保田悦也 <instrctr.etsuya@gmail.com>
2022-03-13 21:52:37 +0900

    add: 各遷移の作成

---
+++ src/components/BloodType.tsx
@@ -0,0 +1,11 @@
+
+const BloodType = () => {
+  return (
+    <div>
+      <h1>血液型占い</h1>
+      <a href="/">TOP</a>
+    </div>
+  )
+}
+
+export default BloodType
---
+++ src/components/Constellation.tsx
@@ -0,0 +1,11 @@
+
+const Constellation = () => {
+  return (
+    <div>
+      <h1>星座占いです</h1>
+      <a href="/">TOP</a>
+    </div>
+  )
+}
+
+export default Constellation
